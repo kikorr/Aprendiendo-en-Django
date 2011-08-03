a18f1eb8b64ed7145e62bee989f9ed1fc4bea1ac
Francisco Sanchez <kikorr@gmail.com>
2011-08-03 23:45:40 +0200

    modified:   views.py

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ def current_datetime(request):
 	return HttpResponse(html)
 
 def hours_ahead(request,offset):
-#	offset=int(offset)
+	offset=int(offset)
 	dt=datetime.datetime.now()+datetime.timedelta(hours=offset)
 	html="<html><body>In %s hour(s), it will be %s.</body></html>" %(offset, dt)
 	return HttpResponse(html)
